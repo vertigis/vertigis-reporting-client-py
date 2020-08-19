4c70552049ec6bdfa3f63858d171516d0bbcd8c9
Ian Schmitz <ianschmitz@gmail.com>
2020-08-19 15:10:10 -0700

    First test passing

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,13 +5,13 @@ All commands are run from within the root of this folder.
 ### Create the virtual environment folder:
 
 ```sh
-$ python3 -m venv ./venv
+$ python3 -m venv .venv
 ```
 
 ### Activate the virtual environment
 
 ```sh
-$ source ./venv/bin/activate
+$ source .venv/bin/activate
 ```
 
 ### Check python binaries are the correct path
@@ -28,36 +28,10 @@ $ source ./venv/bin/activate
 (venv) $ deactivate
 ```
 
-## Managing dependencies
+## Installing dependencies
 
-### Installing packages
-
-Within an activated environment
+To install both the runtime and development dependencies:
 
 ```sh
-(venv) $ pip install numpy
-(venv) $ pip list
-Package    Version
----------- -------
-numpy      1.17.0
-pip        20.1.1
-setuptools 47.1.0
-```
-
-If you open up the `venv/lib/pythonx.x/site-package` directory you can see that `numpy` is installed within this folder but not within the global namespace which is exactly what we want.
-
-### Tracking project dependencies
-
-In order to tell our co-workers what packages our project depends on, we would usually use a `requirements.txt` file within the root of the project folder.
-
-To do that we can actually use the `pip freeze` command and pipe it to the requirements.txt file:
-
-```sh
-(venv) $ pip freeze > requirements.txt
-```
-
-Finally, when a co-worker receives a copy of your project source code, he will need to install the dependencies. To do that he will spin up his own virtual environment and then run:
-
-```sh
-(venv) $ pip install -r requirements.txt
+(venv) $ pip install -e ".[dev]"
 ```
--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -4,41 +4,35 @@ import websockets
 
 from unittest import mock
 
-import geocortex.reporting
+from geocortex.reporting.client import runReport
+
+MOCK_TICKET = "eyJhbGciOiJub25lIiwiemlwIjoiREVGIn0.q1ZKzs9TsqpWUimpLEhVslLyKi8JyUzOTi1xzs8rSc0rUdJRyspP8kwBSeUnFRvpp6YkmiYaGluYpFmmmpibGSZaJienGJqYGpiaGSeZp5gr1dYCAA."
+MOCK_TAG = "e88aca6843c346acbd5999d9c017a587"
 
 
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, url, json_data, status_code):
-            self.url = url
-            self.json_data = json_data
-            self.status_code = status_code
+class MockResponse:
+    def __init__(self, url, json_data, status_code):
+        self.url = url
+        self.json_data = json_data
+        self.status_code = status_code
 
-        def raise_for_status(self):
-            http_error_msg = ""
-            if 400 <= self.status_code < 500:
-                http_error_msg = f"{self.status_code} Client Error for url: {self.url}"
-            elif 500 <= self.status_code < 600:
-                http_error_msg = f"{self.status_code} Server Error for url: {self.url}"
+    def raise_for_status(self):
+        http_error_msg = ""
+        if 400 <= self.status_code < 500:
+            http_error_msg = f"{self.status_code} Client Error for url: {self.url}"
+        elif 500 <= self.status_code < 600:
+            http_error_msg = f"{self.status_code} Server Error for url: {self.url}"
 
-            if http_error_msg:
-                raise requests.exceptions.HTTPError(http_error_msg)
+        if http_error_msg:
+            raise requests.exceptions.HTTPError(http_error_msg)
 
-        def json(self):
-            return self.json_data
+    def json(self):
+        return self.json_data
 
-    responses = {
-        "https://www.arcgis.com/sharing/rest/content/items/abc123?f=json": {
-            "json": {
-                "access": "public",
-                "url": "https://apps.geocortex.com/reporting/",
-            },
-            "status_code": 200,
-        }
-    }
 
+def mocked_request(defaultResponses, args, kwargs):
     otherResponses = kwargs.get("responses", {})
-    responses.update(otherResponses)
+    responses = {**defaultResponses, **otherResponses}
 
     url = args[0]
 
@@ -51,15 +45,58 @@ def mocked_requests_get(*args, **kwargs):
     return MockResponse(url, None, 404)
 
 
+def mocked_requests_get(*args, **kwargs):
+    defaultResponses = {
+        "https://www.arcgis.com/sharing/rest/content/items/abc123?f=json": {
+            "json": {
+                "access": "public",
+                "url": "https://apps.geocortex.com/reporting/",
+            },
+            "status_code": 200,
+        },
+        f"https://apps.geocortex.com/reporting/service/job/artifacts?ticket={MOCK_TICKET}": {
+            "json": {
+                "results": [
+                    {
+                        "$type": "JobResult",
+                        "contentType": "application/pdf",
+                        "tag": MOCK_TAG,
+                        "length": 24003,
+                    },
+                    {"$type": "JobQuit", "kind": "Run"},
+                ],
+            },
+            "status_code": 200,
+        },
+    }
+
+    return mocked_request(defaultResponses, args, kwargs)
+
+
+def mocked_requests_post(*args, **kwargs):
+    defaultResponses = {
+        "https://apps.geocortex.com/reporting/service/job/run": {
+            "json": {"response": {"$type": "TokenResponse", "ticket": MOCK_TICKET}},
+            "status_code": 200,
+        },
+    }
+
+    return mocked_request(defaultResponses, args, kwargs)
+
+
 # def mocked_websockets_connect(*args, **kwargs):
 #     class MockWebsocketConnect
 
 
 class TestReporting(unittest.IsolatedAsyncioTestCase):
     @mock.patch("requests.get", side_effect=mocked_requests_get)
-    async def test_foo(self, mock_get):
-        report = await geocortex.reporting.runReport("abc123", usePolling=True)
-        # self.assertEqual(1, 2)
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    async def test_polling(self, mock_get, mock_post):
+        report = await runReport("abc123", usePolling=True)
+        self.assertEqual(
+            report,
+            f"https://apps.geocortex.com/reporting/service/job/result?ticket={MOCK_TICKET}&tag={MOCK_TAG}",
+        )
 
 
 if __name__ == "__main__":
