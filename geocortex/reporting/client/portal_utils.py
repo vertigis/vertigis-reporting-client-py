8b0ba641279774dd09be94f2c44c5c970dcc6392
Darren Boss <dboss@latitudegeo.com>
2020-09-24 13:35:10 -0700

    - use token when fetching portal item - send portalUrl when fetching service token

--- geocortex/reporting/client/portal_utils.py
+++ geocortex/reporting/client/portal_utils.py
@@ -5,15 +5,18 @@ def _get_portal_rest_url(portal_url: str) -> str:
     return f"{portal_url}/sharing/rest"
 
 
-def _get_portal_item_url(item_id: str, portal_url: str) -> str:
-    return f"{_get_portal_rest_url(portal_url)}/content/items/{item_id}"
+def _get_portal_item_url(item_id: str, portal_url: str, token: str) -> str:
+    url = f"{_get_portal_rest_url(portal_url)}/content/items/{item_id}?f=json"
+    url = url if token is None else f"{url}&token={token}"
+
+    return url
 
 
-def get_portal_item(item_id: str, portal_url: str):
+def get_portal_item(item_id: str, portal_url: str, token: str):
     """Retrieve a portal item by id."""
 
-    item_url = _get_portal_item_url(item_id, portal_url)
-    response = requests.get(f"{item_url}?f=json")
+    item_url = _get_portal_item_url(item_id, portal_url, token)
+    response = requests.get(item_url)
     response.raise_for_status()
     portal_item = response.json()
 
